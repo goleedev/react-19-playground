06165036c205ddd8ee7e961a257cb86e2f9516b0
GO <golee.dev@gmail.com>
2024-09-05 17:51:52 +0900

    chore: Update App.tsx to include new components and remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Suspense } from 'react';
+import Theme from './components/theme';
+import Users from './components/users';
+import Optimistic from './components/optimistic';
+import FormStatus from './components/form-status';
+import FormState from './components/form-state';
+import Action from './actions/form-action';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Suspense
+          fallback={
+            <h1 className="text-2xl text-center font-bold mt-5">Loading...</h1>
+          }
+        >
+          <Action />
+          <FormState />
+          <FormStatus />
+          <Optimistic />
+          <Users />
+          <Theme />
+        </Suspense>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
